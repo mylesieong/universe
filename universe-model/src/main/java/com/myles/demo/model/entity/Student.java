067463953acee906d424d58070806ec5c1e97bb0
Myles Ieong <Myles Ieong>
2017-04-10 00:49:57 +0800

    Add the fieldGroup commit to auto-bind the field to POJO

--- universe-model/src/main/java/com/myles/demo/model/entity/Student.java
+++ universe-model/src/main/java/com/myles/demo/model/entity/Student.java
@@ -2,31 +2,31 @@ package com.myles.demo.model.entity;
 
 public class Student {
 
-    private Integer mId;
-    private String mFirstName;
-    private String mLastName;
-    private Integer mAge;
-    private String mGender;
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String gender;
 
     public Student(){}
 
-    public Integer getId(){return this.mId;}
-    public void setId(Integer id){this.mId = id;}
+    public Integer getId(){return this.id;}
+    public void setId(Integer id){this.id = id;}
 
-    public String getFirstName(){return this.mFirstName;}
-    public void setFirstName(String firstName){this.mFirstName = firstName;}
+    public String getFirstName(){return this.firstName;}
+    public void setFirstName(String firstName){this.firstName = firstName;}
 
-    public String getLastName(){return this.mLastName;}
-    public void setLastName(String lastName){this.mLastName = lastName;}
+    public String getLastName(){return this.lastName;}
+    public void setLastName(String lastName){this.lastName = lastName;}
 
-    public Integer getAge(){return this.mAge;}
-    public void setAge(Integer age){this.mAge = age;}
+    public Integer getAge(){return this.age;}
+    public void setAge(Integer age){this.age = age;}
 
-    public String getGender(){return this.mGender;}
-    public void setGender(String gender){this.mGender = gender;}
+    public String getGender(){return this.gender;}
+    public void setGender(String gender){this.gender = gender;}
 
     @Override 
     public String toString(){
-        return this.mFirstName + "-" + this.mLastName + "-" + this.mAge;
+        return this.firstName + "-" + this.lastName + "-" + this.age;
     }
 }
--- universe-ui/src/main/java/com/myles/demo/ui/students/AddStudentMainLayoutFactory.java
+++ universe-ui/src/main/java/com/myles/demo/ui/students/AddStudentMainLayoutFactory.java
@@ -4,12 +4,16 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.themes.ValoTheme;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.fieldgroup.PropertyId;
 
 import com.myles.demo.model.entity.Student;
 import com.myles.demo.utils.Gender;
@@ -18,9 +22,11 @@ import com.myles.demo.utils.StudentsStringUtils;
 @org.springframework.stereotype.Component
 public class AddStudentMainLayoutFactory{
 
-    private class AddStudentMainLayout extends VerticalLayout{
+    private class AddStudentMainLayout extends VerticalLayout implements Button.ClickListener{
         /* UI component */
+        @PropertyId("firstName")        //Note if is mFirstName, then setter shd name setMFirst...
         private TextField firstName;
+        @PropertyId("lastName")         //in fact this is optional if name is same
         private TextField lastName;
         private TextField age;
         private ComboBox gender;
@@ -45,6 +51,9 @@ public class AddStudentMainLayoutFactory{
 
             saveButton.setStyleName(ValoTheme.BUTTON_FRIENDLY);
             clearButton.setStyleName(ValoTheme.BUTTON_PRIMARY);
+
+            saveButton.addClickListener(this);
+            clearButton.addClickListener(this);
 
             gender.addItem(Gender.GENDER_MALE.getString());
             gender.addItem(Gender.GENDER_FEMALE.getString());
@@ -80,6 +89,31 @@ public class AddStudentMainLayoutFactory{
             gridLayout.addComponent(new HorizontalLayout(saveButton, clearButton), 0, 2);
 
             return gridLayout;
+        }
+
+        @Override
+        public void buttonClick(ClickEvent event){
+            if (event.getSource() == this.saveButton){
+                save();
+            }else if (event.getSource() == this.clearButton){
+                clearField();
+            }
+        }
+
+        private void save(){
+            try{
+                fieldGroup.commit();
+            }catch (CommitException e){
+                e.printStackTrace();
+            }
+            System.out.println(student);  //debug
+        }
+
+        private void clearField(){
+            firstName.setValue(null);
+            lastName.setValue(null);
+            gender.setValue(null);
+            age.setValue(null);
         }
     }
 
