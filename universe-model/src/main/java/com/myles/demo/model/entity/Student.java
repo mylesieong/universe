9e2ed186e63e4839e37dce19d0a103cbf15556c2
Myles Ieong <Myles Ieong>
2017-04-13 23:58:25 +0800

    Database: 1. Add annotation in model class Student for JPA

--- universe-model/src/main/java/com/myles/demo/model/entity/Student.java
+++ universe-model/src/main/java/com/myles/demo/model/entity/Student.java
@@ -4,22 +4,37 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+
+@Entity
+@Table(name="STUDENT")
 public class Student {
 
+    @Id
+    @GeneratedValue
+    @Column(name="id")
     private Integer id;
 
     @NotNull(message="You have to specify first name")
+    @Column(name="first_name")
     private String firstName;
 
     @NotNull(message="You have to specify last name")
+    @Column(name="last_name")
     private String lastName;
 
     @NotNull(message="You have to specify age")
     @Min(value=0, message="Minimum value is 0")
     @Max(value=100, message="Maximum value is 100")
+    @Column(name="age")
     private Integer age;
 
     @NotNull(message="Gender must be set.")
+    @Column(name="gender")
     private String gender;
 
     public Student(){}
